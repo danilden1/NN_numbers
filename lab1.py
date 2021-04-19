164fa06cb59f68c5c22c863ed11a5e0d9a2dd565
Sidorenko Danil <danilden1@yandex.ru>
2021-04-19 14:55:39 +0300

    HELP ME

--- lab1.py
+++ lab1.py
@@ -54,11 +54,19 @@ def load_data():
 		img_l = img_resize.convert(mode="L")
 		img_inverse = PIL.ImageOps.invert(img_l)
 		img_arr = numpy.array(img_inverse)
+		num_pixels = img_arr.shape[0] * img_arr.shape[1]
+
+		# normalize inputs from 0-255 to 0-1
+		img_arr = img_arr / 255
+
+		# one hot encode outputs
+		#img_arr = np_utils.to_categorical(img_arr)
 
 		print("> loaded %s %s %s" % (filename, img_arr.shape, img_arr.dtype))
-		data_list.append(img_resize)
+		data_list.append(img_arr)
 		#data_list[i].show()
 		#i = i + 1
+
 	img_l.show()
 	img_inverse.show()
 
@@ -79,11 +87,8 @@ def load_data():
 # load data
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-print("origin data:")
-print(X_test[1].shape)
-print(X_test[1].dtype)
-print("my data:")
-load_data()
+
+
 # flatten 28*28 images to a 784 vector for each image
 num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
@@ -98,11 +103,19 @@ y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 
+
+data_arr = numpy.asarray(data_list)
+
+print("origin data:")
+print(X_test[1].shape)
+print(X_test[1].dtype)
+print("my data:")
+load_data()
 # build the model
 model = baseline_model()
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+#model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+#scores = model.evaluate(X_test, y_test, verbose=0)
+#print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
