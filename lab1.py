a1e2b598ae9cc253e825936bb4aaccaed9f1a829
Sidorenko Danil <danilden1@yandex.ru>
2021-04-19 13:32:34 +0300

    prepare img for neural network analice

--- lab1.py
+++ lab1.py
@@ -1,19 +1,26 @@
 # Plot ad hoc mnist instances
 from keras.datasets import mnist
-import matplotlib.pyplot as plt
+
+from matplotlib import pyplot
+from matplotlib import image
+from os import listdir
+#import opencv as cv
+from PIL import Image
+import PIL.ImageOps
 # load (downloaded if needed) the MNIST dataset
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # plot 4 images as gray scale
-plt.subplot(221)
-plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
-plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
-plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
-plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
+#plt.subplot(221)
+#plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
+#plt.subplot(222)
+#plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
+#plt.subplot(223)
+#plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
+#plt.subplot(224)
+#plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
 # show the plot
-plt.show()
+#plt.show()
+
 
 
 import numpy
@@ -37,9 +44,46 @@ def baseline_model():
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+data_list = list()
 
+def load_data():
+	i = 0
+	for filename in listdir("data"):
+		img_data = Image.open("data/" + filename)
+		img_resize = img_data.resize((28, 28))
+		img_l = img_resize.convert(mode="L")
+		img_inverse = PIL.ImageOps.invert(img_l)
+		img_arr = numpy.array(img_inverse)
+
+		print("> loaded %s %s %s" % (filename, img_arr.shape, img_arr.dtype))
+		data_list.append(img_resize)
+		#data_list[i].show()
+		#i = i + 1
+	img_l.show()
+	img_inverse.show()
+
+	#data = Image.open('data/image_0.jpg')
+	#data = image.imread('data/image_0.jpg')
+
+	#print()
+	#print(data.size)
+	#data_resize = data.resize((28, 28))
+
+	#print(data_resize.size)
+	#data_resize.show()
+
+
+
+
+
+# load data
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print("origin data:")
+print(X_test[1].shape)
+print(X_test[1].dtype)
+print("my data:")
+load_data()
 # flatten 28*28 images to a 784 vector for each image
 num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
