eead577d4f95a0f649be83f29460cb242a3824ee
Sidorenko Danil <danilden1@yandex.ru>
2021-04-26 12:48:43 +0300

    only 5 numbers again

--- lab1.py
+++ lab1.py
@@ -10,39 +10,30 @@ from skimage.morphology import dilation
 import PIL.ImageOps
 from prettytable import PrettyTable
 # load (downloaded if needed) the MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
 # plot 4 images as gray scale
 
 
 
 
-import numpy
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.utils import np_utils
+import tensorflow.keras as keras
 
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
+from keras import models
+from keras import layers
 
 # define baseline model
 def baseline_model():
 	# create model
-	model = Sequential()
-	#6-уровневая сеть 784-50-100-500-1000-10-10
-	model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
-	"""
-	model.add(Dense(50, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(100, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(500, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(1000, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(10, kernel_initializer='normal', activation='relu'))
-	"""
-	model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model = models.Sequential([
+		layers.Conv2D(32, (3, 3), activation='relu',  # (3,3) - фильтр
+					  input_shape=(28, 28, 1)),
+		layers.MaxPooling2D((2, 2)),  # фильтр (2,2) для пулинга
+		layers.Conv2D(64, (3, 3), activation='relu'),
+		layers.MaxPooling2D((2, 2)),
+		layers.Conv2D(64, (3, 3), activation='relu'),
+		layers.Flatten(),
+		layers.Dense(64, 'relu'),
+		layers.Dense(10, 'softmax')
+	])
 	return model
 
 def delete_background(img, threshold):
@@ -57,7 +48,9 @@ def delete_background(img, threshold):
 
 
 data_list = list()
-
+import numpy
+seed = 7
+numpy.random.seed(seed)
 def load_data():
 	for filename in listdir("data"):
 		img_data = Image.open("data/" + filename)
@@ -80,62 +73,64 @@ def load_data():
 
 
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
+import matplotlib.pyplot as plt
+plt.imshow(x_train[0, :, :])
+plt.colorbar()
 
+x_train = x_train.reshape((60000, 28, 28, 1))
+x_train = x_train.astype('float32') / 255
+x_test = x_test.reshape((10000, 28, 28, 1))
+x_test = x_test.astype('float32') / 255
 
-#pyplot.imshow(X_train[0], cmap=pyplot.get_cmap('gray'))
-#pyplot.subplot(224)
-#pyplot.imshow(X_train[1], cmap=pyplot.get_cmap('gray'))
-# show the plot
+from keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
+from keras import models
+from keras import layers
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', # (3,3) - фильтр
+                        input_shape=(28,28,1)),
+    layers.MaxPooling2D((2,2)), # фильтр (2,2) для пулинга
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, 'relu'),
+    layers.Dense(10, 'softmax')
+])
 
+from datetime import datetime
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+log_dir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+model.fit(x=x_train,
+          y=y_train,
+          epochs=1,
+          batch_size=64,
+          validation_data=(x_test, y_test),
+          callbacks=[tensorboard_callback])
 
-
-#X_test[0].show()
-
-# flatten 28*28 images to a 784 vector for each image
-num_pixels = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
-print(X_train.shape)
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-
-
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
-
-
-print("origin data:")
-print(X_test[1].shape)
-print(X_test[1].dtype)
-print("my data:")
-
-
-# build the model
-model = baseline_model()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10 , batch_size=20, verbose=1)
-
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
+scores = model.evaluate(x_test, y_test, verbose=0)
 print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
 load_data()
+
 data_x = numpy.asarray(data_list)
 data_y = numpy.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-num_pixels_my = data_x.shape[1] * data_x.shape[2]
-data_x = data_x.reshape(data_x.shape[0], num_pixels_my).astype('float32')
+data_x = data_x.reshape((10, 28, 28, 1))
+data_x = data_x.astype('float32') / 255
 
-data_x = data_x / 255
-
+from keras.utils import np_utils
 data_y = np_utils.to_categorical(data_y)
 print("my model")
 #scores = model.evaluate(data_x, data_y)
+
 prediction = model.predict(data_x)
 
 t = PrettyTable(['Numb', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
@@ -172,6 +167,7 @@ for i in range(0, 10):
 
 
 pyplot.show()
+
 
 
 
